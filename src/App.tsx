f089110fbedcb78b5dadafa9ce8f0301134a2d84
JannikU09 <j.uphoff@innfactory.de>
2026-01-09 11:58:42 +0100

    Button hinzugefügt, um ToDo's wieder zu löschen. Dies funktioniert allerdings noch nicht richtig.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { todosAtom, addTodoAtom } from "./store/todoStore";
+import { todosAtom, addTodoAtom, deleteTodoAtom } from "./store/todoStore";
 import { useAtomValue, useSetAtom } from "jotai";
 
 
@@ -6,6 +6,7 @@ function App() {
 
   const todos = useAtomValue(todosAtom);
   const addTodo = useSetAtom(addTodoAtom);
+  const deleteTodo = useSetAtom(deleteTodoAtom)
 
   const handleAddTodo = () => {
     const neueTodo = document.getElementById("inputFeld") as HTMLInputElement;
@@ -15,14 +16,19 @@ function App() {
     }
   }
 
+  const handleDeleteTodo = () => {
+    deleteTodo(1);
+  }
+
   return (
     <div>
-      <input type="text" placeholder="test" id="inputFeld" />
+      <input type="text" placeholder="Neue ToDo" id="inputFeld" />
       <button type="button" onClick={handleAddTodo}>hinzufügen</button>
       {todos.map((todo) => (
         <>
           <div>{todo.text}</div>
           <div>Id: {todo.id}</div>
+          <button type="button" onClick={handleDeleteTodo}>entfernen</button>
         </>
       ))}
     </div>
