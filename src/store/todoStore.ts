2a38071ef2da0eadee90df78f5c27c57a1d643d1
JannikU09 <j.uphoff@innfactory.de>
2026-01-09 11:06:23 +0100

    feat: Jede ToDo bekommt ihre eigene Id.

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { atom } from "jotai";
 
 export interface ToDo {
-    id: string;
+    id: number;
     isChecked: boolean;
     text: string;
 }
@@ -11,11 +12,27 @@ export const todosAtom = atom<ToDo[]>([]);
 export const addTodoAtom = atom(
     null,
     (get, set, text: string) => {
+
+        const todoListe = get(todosAtom);
+        const neueId = todoListe.length + 1;
+
         const todo: ToDo = {
-            id: "1",
+            id: neueId,
             text,
             isChecked: false,
         };
+
         set(todosAtom, [...get(todosAtom), todo])
     }
+
+
+);
+
+export const updateTodoAtom = atom(
+
+);
+
+
+export const deleteTodoAtom = atom(
+
 )
