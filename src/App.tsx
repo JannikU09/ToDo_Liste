b083cb4b076f0f957646898f242dd9f370e766dd
JannikU09 <j.uphoff@innfactory.de>
2026-01-12 15:51:58 +0100

    Wenn eine ToDo jetzt abgehackt wird, wird der Text durchgestrichen.
    Hinzufügen der Funktion, um eine ToDo zu bearbeiten. Wenn auf bearbeiten gedrückt wird ändert sich der Text der ToDo, die man bearbeiten will. Aktuell wird die zu erstbearbeitete Todo der zugehörige Text weiter übernommen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { todosAtom, addTodoAtom, deleteTodoAtom, updateTodoAtom } from "./store/todoStore";
 import { useAtomValue, useSetAtom } from "jotai";
 
@@ -8,6 +9,7 @@ function App() {
   const addTodo = useSetAtom(addTodoAtom);
   const deleteTodo = useSetAtom(deleteTodoAtom);
   const updateTodo = useSetAtom(updateTodoAtom);
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleAddTodo = () => {
     const neueTodo = document.getElementById("inputFeld") as HTMLInputElement;
@@ -15,35 +17,47 @@ function App() {
       const todoText: string = neueTodo.value;
       addTodo(todoText);
     }
-  }
+  };
 
-  const handleUpdateTodo = (e: React.MouseEvent<HTMLElement>) => {
-    const target = e.target as Element;
+  const handleUpdateTodo = (event: React.MouseEvent<HTMLElement>) => {
+    const bearbeiteteTodo = document.getElementById("updateText") as HTMLInputElement;
+    todos.map((todo) => todo.text = bearbeiteteTodo.value)
+    const upgedatedTodo: string = bearbeiteteTodo.value;
+    const target = event.target as Element;
     const id = target.id;
-    updateTodo(parseInt(id))
-  }
+    updateTodo(upgedatedTodo, parseInt(id));
+  };
 
-  const handleDeleteTodo = (e: React.MouseEvent<HTMLElement>) => {
-    const target = e.target as Element;
+
+  const handleDeleteTodo = (event: React.MouseEvent<HTMLElement>) => {
+    const target = event.target as Element;
     const id = target.id;
+    console.log(id);
     deleteTodo(parseInt(id));
   };
+
+  const handleCheckedTodo = () => {
+    setIsChecked(!isChecked);
+  }
 
   return (
     <div>
       <input type="text" placeholder="Neue ToDo" id="inputFeld" />
-      <button type="button" onClick={handleAddTodo}>hinzufügen</button>
+      <button type="button" onClick={handleAddTodo}>Hinzufügen</button>
       {todos.map((todo) => (
         <>
+          <div />
+          <input type="checkbox" id="checkbox" checked={isChecked} onChange={handleCheckedTodo} />
+          <label htmlFor="checkbox" id="textTodo" style={{ textDecoration: isChecked ? "line-through" : "none" }}>{todo.text}</label>
           <div>Id: {todo.id}</div>
-          <div>{todo.text}</div>
-          <input type="text" placeholder="bearbeiten" />
-          <button type="button" onClick={handleUpdateTodo} id={`${todo.id}`}>bearbeiten</button>
-          <button type="button" onClick={handleDeleteTodo} id={`${todo.id}`}>entfernen</button>
+          <div />
+          <input type="text" placeholder={todo.text} id="updateText" />
+          <button type="button" onClick={handleUpdateTodo}>bearbeiten</button>
+          <button type="button" onClick={handleDeleteTodo} id={`${todo.id}`}>löschen</button>
         </>
       ))}
     </div>
   )
 }
 
-export default App
+export default App;
