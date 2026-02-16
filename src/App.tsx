74180118be4cb83182ca92b5f49c8d3468cc9b1f
Roland Wallner <rolandolah@icloud.com>
2026-02-16 09:40:14 +0100

    feat: Add biome.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import { todosAtom, addTodoAtom, deleteTodoAtom, updateTodoAtom } from "./store/todoStore";
 import { useAtomValue, useSetAtom } from "jotai";
-import "./App.css"
-
+import { useState } from "react";
+import { addTodoAtom, deleteTodoAtom, todosAtom, updateTodoAtom } from "./store/todoStore";
+import "./App.css";
 
 function App() {
-
   const todos = useAtomValue(todosAtom);
   const addTodo = useSetAtom(addTodoAtom);
   const deleteTodo = useSetAtom(deleteTodoAtom);
@@ -18,7 +16,6 @@ function App() {
     setNewTodoInput("");
   };
 
-
   return (
     <div>
       <input
@@ -28,41 +25,50 @@ function App() {
         id="inputFeld"
         onChange={(event) => setNewTodoInput(event.target.value)}
       />
-      <button type="button" onClick={(handleAddTodo)}>Hinzufügen</button>
+      <button type="button" onClick={handleAddTodo}>
+        Hinzufügen
+      </button>
 
       {todos.map((todo) => (
-
         <li key={todo.id}>
           <input
             type="checkbox"
             id={`${todo.id}`}
-            onClick={() => updateTodo({
-              ...todo,
-              isChecked: !todo.isChecked
-            })} />
-          <label htmlFor={`${todo.id}`} id="textTodo" style={{ textDecoration: todo.isChecked ? "line-through" : "none" }}>{todo.text}</label>
+            onClick={() =>
+              updateTodo({
+                ...todo,
+                isChecked: !todo.isChecked,
+              })
+            }
+          />
+          <label
+            htmlFor={`${todo.id}`}
+            id="textTodo"
+            style={{ textDecoration: todo.isChecked ? "line-through" : "none" }}
+          >
+            {todo.text}
+          </label>
           <div>Id: {todo.id}</div>
 
           <form>
             <textarea
               placeholder={todo.text}
-              onChange={(event) => updateTodo({
-                ...todo,
-                text: event.target.value
-              })}
+              onChange={(event) =>
+                updateTodo({
+                  ...todo,
+                  text: event.target.value,
+                })
+              }
               className="updateInput"
             />
           </form>
-          <button
-            type="button"
-            onClick={() => deleteTodo(todo.id)}
-            id={`${todo.id}`}>
+          <button type="button" onClick={() => deleteTodo(todo.id)} id={`${todo.id}`}>
             löschen
           </button>
         </li>
       ))}
     </div>
   );
-};
+}
 
 export default App;
--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,45 +1,33 @@
 import { atom } from "jotai";
 
 export interface ToDo {
-    id: number;
-    isChecked: boolean;
-    text: string;
+  id: number;
+  isChecked: boolean;
+  text: string;
 }
 
 export const todosAtom = atom<ToDo[]>([]);
 
-export const addTodoAtom = atom(
-    null,
-    (get, set, text: string) => {
+export const addTodoAtom = atom(null, (get, set, text: string) => {
+  const id = Date.now();
+  const todo: ToDo = {
+    id,
+    text,
+    isChecked: false,
+  };
 
-        const id = Date.now();
-        const todo: ToDo = {
-            id,
-            text,
-            isChecked: false,
-        };
+  set(todosAtom, [...get(todosAtom), todo]);
+});
 
-        set(todosAtom, [...get(todosAtom), todo]);
-    }
-);
+export const updateTodoAtom = atom(null, (get, set, updatedTodo: ToDo) => {
+  const todos = get(todosAtom);
+  const updatedTodos = todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo));
 
-export const updateTodoAtom = atom(
-    null,
-    (get, set, updatedTodo: ToDo) => {
+  set(todosAtom, updatedTodos);
+});
 
-        const todos = get(todosAtom);
-        const updatedTodos = todos.map((todo) => todo.id === updatedTodo.id ? updatedTodo : todo);
-
-        set(todosAtom, updatedTodos);
-    }
-);
-
-
-export const deleteTodoAtom = atom(
-    null,
-    (get, set, id: number) => {
-        const todos = get(todosAtom);
-        const deleted = todos.filter(todo => todo.id !== id);
-        set(todosAtom, deleted);
-    }
-);
+export const deleteTodoAtom = atom(null, (get, set, id: number) => {
+  const todos = get(todosAtom);
+  const deleted = todos.filter((todo) => todo.id !== id);
+  set(todosAtom, deleted);
+});
