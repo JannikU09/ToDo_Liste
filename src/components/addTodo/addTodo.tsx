4a191f4ce597ad696dd83fd20d5e72c12f319bb2
JannikU09 <j.uphoff@innfactory.de>
2026-02-17 15:50:07 +0100

    AddTodo() in eine eigene Datei geschoben, um diese dort aufzurufen.

---
+++ src/components/addTodo/addTodo.tsx
@@ -0,0 +1,40 @@
+import { useSetAtom } from "jotai";
+import { useState } from "react";
+import { addTodoAtom, category } from "../../store/todoStore";
+import "./addTodo.css";
+
+export const AddTodos = () => {
+    const addTodo = useSetAtom(addTodoAtom);
+
+    const [newTodoInput, setNewTodoInput] = useState("");
+    const [newCategory, setNewCategory] = useState("");
+
+    const handleAddTodo = () => {
+        addTodo(newTodoInput, newCategory);
+        setNewCategory("");
+    };
+
+    return (
+        <div className="addTodo">
+            {/* Input für eine Neue ToDo */}
+            <input
+                value={newTodoInput}
+                type="text"
+                placeholder="Neue ToDo"
+                id="inputFeld"
+                onChange={(event) => setNewTodoInput(event.target.value)}
+            />
+            
+            <select id="dropdown" onChange={(event) => setNewCategory(event.target.value)}>
+                {category.map((categories) => (
+                    <option key={categories.id} value={categories.id}>{categories.label}</option>
+                ))}
+            </select>
+            <button type="button" onClick={handleAddTodo}>
+                Hinzufügen
+            </button>
+
+        </div>
+    )
+
+}
